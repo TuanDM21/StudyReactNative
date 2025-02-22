6744bb859d2baa694fc45bf630853a954d43d343
tinhnguyen0110 <tinh2kqb@gmail.com>
2025-02-22 18:52:02 +0700

    Xu ly su kien

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+
 
 export default function App() {
+
+  const [todoText, setTodoText] = useState('');
+
+  function textInputChanged(textChange){
+    setTodoText(textChange);
+  }
+
+  function addTodo(){
+    console.log(todoText)
+  } 
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.TextInput} placeholder='your todo' />
-        <Button title="Add todo" />
+        <TextInput onChangeText={textInputChanged} style={styles.TextInput} placeholder='your todo' />
+        <Button onPress={addTodo} title="Add todo" />
       </View>
-      <View>
-        <Text content='Todo 1' >The todo list...</Text>
+      <View style={styles.todoList}>
+        <Text>The todo list...</Text>
       </View>
     </View>
   );
@@ -19,15 +32,25 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
     paddingTop: 100,
+    flex : 1
   },
   inputContainer: {
     flexDirection: 'row',
+    marginBottom : 50,
+    borderBottomWidth :1,
+    borderBottomColor: 'grey',
+    paddingBottom : 50,
+    flex : 1,
+    alignItems :'center',
   },
   TextInput: {
     borderWidth: 2,
-    borderColor: 'black',
+    borderColor: 'blue',
     padding: 8,
     marginRight: 8,
     width: '70%',
   },
+  todoList: {
+    flex : 4
+  }
 });
